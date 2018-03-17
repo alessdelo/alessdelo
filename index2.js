f2e25906e0867a7307aa0bccee273640ec6b0b86
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-17 08:53:30 +0100

    Update index2.js
    ispired by https://shockoe.com/blog/creating-dynamic-web-pages-ejs/

--- index2.js
+++ index2.js
@@ -1,11 +1,23 @@
+// https://shockoe.com/blog/creating-dynamic-web-pages-ejs/
+
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const router = express.Router()
+
+const header = "header1.ejs"
+const navbar = "nav1.ejs"
+const footer = "footer.ejs"
+
+
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  /*
   .get('/', (req, res) => res.render('pages/index2'))
     .get('/pag2', (req, res) => res.render('pages/pag2'))
     .get('/pag3', (req, res) => res.render('pages/pag3'))
@@ -13,3 +25,10 @@ express()
     .get('/pag5', (req, res) => res.render('pages/pag5'))
 .post('bla', (req, res) => res.render('pages/post'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  */
+
+router.get('/', function(req, res, next) {
+  res.render('pages/index2', {title: 'Router test' })
+  })
+
+module.exports=router
