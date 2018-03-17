5df96c1150da99c38e7320282eb2ff96c7a34661
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-17 08:30:41 +0100

    Create index2.js
    New version of index for dynamic content pages

---
+++ index2.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index2'))
+    .get('/pag2', (req, res) => res.render('pages/pag2'))
+    .get('/pag3', (req, res) => res.render('pages/pag3'))
+    .get('/pag4', (req, res) => res.render('pages/pag4'))
+    .get('/pag5', (req, res) => res.render('pages/pag5'))
+.post('bla', (req, res) => res.render('pages/post'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
