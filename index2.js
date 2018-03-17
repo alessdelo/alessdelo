3435afd9c2adb97ba0f931e3ef9efb94e00b34aa
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-17 15:52:24 +0100

    Update index2.js

--- index2.js
+++ index2.js
@@ -12,12 +12,12 @@ const footer = "footer.ejs"
 
 
 
-
+/*
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  /*
+  
   .get('/', (req, res) => res.render('pages/index2'))
     .get('/pag2', (req, res) => res.render('pages/pag2'))
     .get('/pag3', (req, res) => res.render('pages/pag3'))
@@ -26,6 +26,11 @@ express()
 .post('bla', (req, res) => res.render('pages/post'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   */
+
+  router.use(express.static(path.join(__dirname, 'public')))
+  router.set('views', path.join(__dirname, 'views'))
+  router.set('view engine', 'ejs')
+
 
 router.get('/', function(req, res, next) {
   res.render('pages/index3', {title: 'Router test' })
