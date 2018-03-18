bdb994b01c9be0a3fe48bd6f137be4f9af1e55d1
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-18 09:51:51 +0100

    Update index2.js

--- index2.js
+++ index2.js
@@ -2,6 +2,7 @@
 
 
 const express = require('express')
+const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const router = express.Router()
@@ -28,8 +29,8 @@ express()
   */
 
   router.use(express.static(path.join(__dirname, 'public')))
-  express.set('views', path.join(__dirname, 'views'))
-  express.set('view engine', 'ejs')
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
 
 
 router.get('/', function(req, res, next) {
