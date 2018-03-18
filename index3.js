94d26cd5e598f42f3704b3c5a4b4ee166981d51b
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-18 21:07:30 +0100

    Update index3.js

--- index3.js
+++ index3.js
@@ -86,9 +86,7 @@ var app = express()
   app.use(express.static(path.join(__dirname, 'public')))
   app.set('views', path.join(__dirname, 'views'))
   app.set('view engine', 'ejs')
- app.get('/', (req, res) => res.render('pages/index3',indexData))
-
- // app.get('/', (req, res) => res.render('pages/index3',indexData))
+// app.get('/', (req, res) => res.render('pages/index3',indexData))
 
  app.get('/', (req, res) => res.render(index,homeData))
 // app.get('/home', (req, res) => res.render('pages/index3',homeData))
