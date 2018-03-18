c9bb0d9ed15cd33a8bdf839d768ecfbf03b7561e
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-18 21:14:20 +0100

    Create index4.js

---
+++ index4.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+var index = 'pages/index4'
+var header = "../partials/header2.ejs"
+var nav = "../partials/nav5.ejs"
+var footer = "../partials/footer1.ejs"
+
+var homeData = {
+  title:'home',
+  content:'../contents/home.ejs', 
+  nav: nav,
+  header: header,
+  footer: footer
+}
+
+var aboutData = {
+  title:'about',
+  content:'../contents/about.ejs',
+  nav: nav,
+  header: header,
+  footer: footer
+}
+
+var productsData = {
+  title:'products',
+  content:'../contents/products.ejs',
+  nav: nav,
+  header: header,
+  footer: footer
+}
+
+var app = express()
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+
+ app.get('/', (req, res) => res.render(index,homeData))
+ app.get('/about', (req, res) => res.render(index,aboutData))
+ app.get('/products', (req, res) => res.render(index,productsData))
+
+ app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
