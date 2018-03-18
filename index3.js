5ef9bf04a1feba6a2d1824f98c42ded8a8414759
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-18 15:24:58 +0100

    Update index3.js

--- index3.js
+++ index3.js
@@ -6,11 +6,14 @@ const PORT = process.env.PORT || 5000
 
 var nav = "../partials/nav4.ejs"
 
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index3', {title:'page two', content:'bla2 bla2...', testfile:'testfile.ejs', nav: nav}))
+  .get('/', (req, res) => res.render('pages/index3', {
+}))
     .get('/pag2', (req, res) => res.render('pages/pag2'))
     .get('/pag3', (req, res) => res.render('pages/pag3'))
     .get('/pag4', (req, res) => res.render('pages/pag4'))
