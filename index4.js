810b866f1b0634b32713fef1a90ee210de65d2b9
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-19 14:33:07 +0100

    Update index4.js

--- index4.js
+++ index4.js
@@ -7,6 +7,7 @@ var header = "../partials/header2.ejs"
 var nav = "../partials/nav5.ejs"
 var footer = "../partials/footer1.ejs"
 
+/*
 var homeData = {
   title:'home',
   content:'../contents/home.ejs', 
@@ -30,14 +31,42 @@ var productsData = {
   header: header,
   footer: footer
 }
+*/
+var pageData = {
+  home: {
+    title:'the home',
+    content:'../contents/home.ejs', 
+    nav: nav,
+    header: header,
+    footer: footer
+  },
+  about: {
+    title:'the about',
+    content:'../contents/about.ejs',
+    nav: nav,
+    header: header,
+    footer: footer
+  },
+  products: {
+    title:'the products',
+    content:'../contents/products.ejs',
+    nav: nav,
+    header: header,
+    footer: footer
+  }
+}
 
 var app = express()
   app.use(express.static(path.join(__dirname, 'public')))
   app.set('views', path.join(__dirname, 'views'))
   app.set('view engine', 'ejs')
-
+/*
  app.get('/', (req, res) => res.render(index,homeData))
  app.get('/about', (req, res) => res.render(index,aboutData))
  app.get('/products', (req, res) => res.render(index,productsData))
+*/
+ app.get('/', (req, res) => res.render(index,pageData.home))
+ app.get('/about', (req, res) => res.render(index,pageData.about))
+ app.get('/products', (req, res) => res.render(index,pageData.products))
 
  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
