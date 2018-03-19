fa5a645057489bb5f3694ca6a3c17c00cd5799cc
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-19 14:34:45 +0100

    Update index4.js

--- index4.js
+++ index4.js
@@ -7,31 +7,7 @@ var header = "../partials/header2.ejs"
 var nav = "../partials/nav5.ejs"
 var footer = "../partials/footer1.ejs"
 
-/*
-var homeData = {
-  title:'home',
-  content:'../contents/home.ejs', 
-  nav: nav,
-  header: header,
-  footer: footer
-}
 
-var aboutData = {
-  title:'about',
-  content:'../contents/about.ejs',
-  nav: nav,
-  header: header,
-  footer: footer
-}
-
-var productsData = {
-  title:'products',
-  content:'../contents/products.ejs',
-  nav: nav,
-  header: header,
-  footer: footer
-}
-*/
 var pageData = {
   home: {
     title:'the home',
@@ -60,11 +36,7 @@ var app = express()
   app.use(express.static(path.join(__dirname, 'public')))
   app.set('views', path.join(__dirname, 'views'))
   app.set('view engine', 'ejs')
-/*
- app.get('/', (req, res) => res.render(index,homeData))
- app.get('/about', (req, res) => res.render(index,aboutData))
- app.get('/products', (req, res) => res.render(index,productsData))
-*/
+
  app.get('/', (req, res) => res.render(index,pageData.home))
  app.get('/about', (req, res) => res.render(index,pageData.about))
  app.get('/products', (req, res) => res.render(index,pageData.products))
