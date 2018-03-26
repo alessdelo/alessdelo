897f3e8c280f21fbdc8b11638d34b714336e59a7
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-26 18:41:20 +0200

    Update index7.js

--- index7.js
+++ index7.js
@@ -22,6 +22,7 @@ mongoose.connect(process.env.MONGOLAB_URI)
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
+  console.log('mongo connected')
   mongoMsg ='mongo connected'
 });
 
