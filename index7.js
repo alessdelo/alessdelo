d4a83bb52927cfbf796cd57ae2ece7d53151405a
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-26 19:08:25 +0200

    Update index7.js

--- index7.js
+++ index7.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGOLAB_URI, function (error) {
 
 // mongoose.connect(process.env.MONGOLAB_URI)
 var uri = 'mongodb://admin:PWD4Admin@ds221339.mlab.com:21339/delotest10'
-mongoose.connect(uri)
+mongoose.connect('cosa')
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
