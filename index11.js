b3e73382e428a3c8c8a5ab67209e9ba1fcd46606
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-30 00:22:34 +0200

    Update index11.js

--- index11.js
+++ index11.js
@@ -246,7 +246,7 @@ app.get('/postdata', function (req, res) {
 
 function showPostData(req, res) {
  
- res.send('POST request to the homepage')
+// res.send('POST request to the homepage')
  
  var postAuthor = req.body.author
  var postTitle = req.body.title
