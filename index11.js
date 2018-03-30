dfdb14d649492b9c4702798aed23c6948669a64d
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-30 14:27:34 +0200

    Update index11.js

--- index11.js
+++ index11.js
@@ -8,7 +8,7 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const assert = require('assert')
-// const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 
 
 const PORT = process.env.PORT || 5000
