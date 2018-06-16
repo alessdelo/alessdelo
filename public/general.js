fa8a6c2fba0591b2c57d661a21bb98539f853796
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-06-16 21:53:54 +0200

    Update general.js

--- public/general.js
+++ public/general.js
@@ -1,5 +1,8 @@
- // PRINTOBJECT
- 
+// **********
+// DEBUG
+// **********
+
+// PRINTOBJECT
  // Fare il print() di un oggetto
             function printObject(o) {
                 var out = '';
@@ -8,3 +11,20 @@
                 }
                 alert(out);
             }
+
+
+// **********
+// DATABASE
+// **********
+
+// RETRIEVES DATE FROM TIMESTAMP
+// var timeStr = "2010-01-13T18:31:16Z";
+function dateFromTimestamp(timeStr) {
+	var date = new Date(timeStr);
+	var day = date.getDate();
+	var year = date.getFullYear();
+	var month = date.getMonth()+1;
+	var dateStr = day + "/"+ month + "/" + year;
+	
+	return dateStr;
+	} 
