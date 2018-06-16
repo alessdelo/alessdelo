a62052c0f21e8a344931ab7a97bc728a569e0221
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-06-16 21:45:54 +0200

    Update infogenerator.js

--- public/infogenerator.js
+++ public/infogenerator.js
@@ -1,23 +1,29 @@
 
+// generates a random string
 function randomStr(m) {
 	var m = m || 9; s = '', r = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 	for (var i=0; i < m; i++) { s += r.charAt(Math.floor(Math.random()*r.length)); }
 	return s;
 }
+
+// 
 function fillCoords(length){
 	var multip = Math.pow(10,length);
 	var randnum = Math.floor(Math.random() * multip);
 	return randnum;
 }
+
+// GENERATES RANDOM COORDINATES FROM EXTREME POINTS
+// example:
+// Area limits (Roma):
+// var coordX = randomIntFromInterval(41.8000000,41.9999999,7);
+// var coordY = randomIntFromInterval(12.3999999,12.6599999,7);
+
 function randomIntFromInterval(min,max,decimals)
 {
   return Number((Math.random()*(max-min)+min).toFixed(decimals));
 }
-// Area limits (Roma):
-// - top-left: 41.9999999, 12.3999999
-// - bot-right: 41.7999999, 12.6599999
-var coordX = randomIntFromInterval(41.8000000,41.9999999,7);
-var coordY = randomIntFromInterval(12.3999999,12.6599999,7);
+
 // retrieves date  from timestamp
 // var timeStr = "2010-01-13T18:31:16Z";
 function dateFromTimestamp(timeStr) {
@@ -29,6 +35,12 @@ function dateFromTimestamp(timeStr) {
 	
 	return dateStr;
 	} 
+
+// GENERATES A RANDOM INT
 	
 	// random number 1-10	
-	var theRate = Math.floor(Math.random() * 10)+1; 
+	
+	    function randomInt( ) {
+			var theInt = Math.floor(Math.random() * 10)+1; 
+			return theInt;
+		}
